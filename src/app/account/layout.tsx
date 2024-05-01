911ca1c635784cfc7b3e4607e4ca6267fd36a788
Rhaqim <anusiemj@gmail.com>
2024-05-01 01:13:52 +0100

    sidenav added

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -1,5 +1,5 @@
+import SideNav from "@/components/Account/SideNav";
 import React from "react";
-
 
 export default function AccountLayout({
 	children,
@@ -7,8 +7,11 @@ export default function AccountLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<div>
-			{children}
+		<div className="flex flex-row">
+			<SideNav />
+			<div className="flex flex-col w-full p-4 min-h-screen overflow-y-auto">
+				{children}
+			</div>
 		</div>
 	);
 }
